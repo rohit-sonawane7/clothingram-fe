f9b9513ff3e4753abff07a97fff1e1eff7f2217f
rohit-sonawane7 <rohitsonawane735@gmail.com>
2025-09-30 00:55:31 +0530

    Fix missing Tailwind color definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   theme: {
     extend: {
       colors: {
+        background: '#f9fafb',
+        foreground: '#111827',
+        border: '#e5e7eb',
         primary: {
           50: '#f0f9ff',
           100: '#e0f2fe',
